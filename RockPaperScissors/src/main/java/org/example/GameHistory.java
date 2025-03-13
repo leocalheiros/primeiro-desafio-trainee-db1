56de5137f9f1fa54b9ed975b506cb1d888caeb8c
Pedro Bruce de Lima <pedrobrucelima@gmail.com>
2025-03-13 11:27:48 -0300

    :hammer: ajustando exibição das últimas rodadas

--- RockPaperScissors/src/main/java/org/example/GameHistory.java
+++ RockPaperScissors/src/main/java/org/example/GameHistory.java
@@ -7,9 +7,6 @@ public class GameHistory {
     private final List<String> history = new ArrayList<>();
 
     public void print() {
-        if (history.size() > 5) {
-            history.removeFirst();
-        }
         System.out.println("\n========== ÚLTIMAS PARTIDAS ==========");
         for (String s : history) {
             System.out.println(s);
@@ -18,5 +15,8 @@ public class GameHistory {
 
     public void add(String player1, String player2, Move move1, Move move2, String winner) {
         history.add(String.format("%s (%s) vs %s (%s): %s", player1, move1, player2, move2, winner));
+        if (history.size() > 5) {
+            history.removeFirst();
+        }
     }
 }
